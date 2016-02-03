1c0c22851094955f74b6cf7d6a57414a252a1dd5
Olexander Kalinichenko <olexkalinichenko@gmail.com>
2016-02-03 18:39:52 +0200

    First commit

---
+++ test1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Богдан Ступка </title>
+</head>
+<body>
+<img src="http://st.kp.yandex.net/images/actor_iphone/iphone360_25255.jpg"/>
+
+
+</p>
+</body>
+</html>
